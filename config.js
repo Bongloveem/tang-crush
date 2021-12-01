e28c8675af2d50d92592a1cd9db6cb35356cb83b
Bongloveem <95288563+Bongloveem@users.noreply.github.com>
2021-12-01 10:01:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Chị 3 ơiiiiii!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    title: 'Phải chăng chị muốn rửa chén dùm em 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy !',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnYes: 'oki cưng chị rửa',
+    btnNo: 'không nhé !',
+    question:'Trên thế giới hơn 7 tỉ người mà sao chị lại chọn rửa chén cho em',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Vì chị thích OK',
+    mess: 'Em biết mà hihihi 😘😘',
+    messDesc: 'Tối nay cũng rửa nhá.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.messenger.com/t/4336718223048555/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
